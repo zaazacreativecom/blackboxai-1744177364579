056d58a02b7521209104156ec6dd37bdc2e35af1
Zaaza Creative <123926981+zaazacreativecom@users.noreply.github.com>
2025-04-09 12:43:03 +0700

    Add complaint-analysis/src/main/java/com/example/complaintanalysis/ComplaintAnalysisApplication.java 1744177382245

---
+++ complaint-analysis/src/main/java/com/example/complaintanalysis/ComplaintAnalysisApplication.java
@@ -0,0 +1,11 @@
+package com.example.complaintanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintAnalysisApplication.class, args);
+    }
+}
